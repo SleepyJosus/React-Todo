96d6f1d9ae8be7db7f78608ad3bf38e0e4bffa3b
Joe Gerhard <jlgerhard@gmail.com>
2019-09-24 13:38:39 -0500

    Refactor code to use child components

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      userName: "Adam",
+      userName: "Joe",
       todoItems: [
         {action: "Buy Flowers", done: false},
         {action: "Get Shoes", done: false},
@@ -22,8 +22,7 @@ export default class App extends Component {
   }
 
   createNewTodo = () => {
-    if (!this.state.todoItems
-      .find(item => item.action === this.state.newItemText)) {
+    if (!this.state.todoItems.find(item => item.action === this.state.newItemText)) {
         this.setState({
           todoItems: [...this.state.todoItems, {
             action: this.state.newItemText, done: false
@@ -32,11 +31,25 @@ export default class App extends Component {
         });
       }
   }
+
+  toggleTodo = (todo) => this.setState({ todoItems: 
+    this.state.todoItems.map(item => item.action === todo.action
+       ? { ...item, done: !item.done } : item) });
+
+  todoTableRows = () => this.state.todoItems.map(item => 
+      <tr key={ item.action }>
+        <td>{ item.action }</td>
+        <td>
+          <input type="checkbox" checked={ item.done }
+            onChange={ () => this.toggleTodo(item) } />
+        </td>
+      </tr> );
   
   render = () =>
     <div>
       <h4 className="bg-primary text-white text-center p-2">
         { this.state.userName }'s To Do List
+        ({ this.state.todoItems.filter(t => !t.done).length} items to do)
       </h4>
       <div className="container-fluid">
         <div className="my-1">
@@ -50,6 +63,12 @@ export default class App extends Component {
             onClick={ this.createNewTodo }>Add
           </button>
         </div>
+        <table className="table table-striped table-bordered">
+          <thead>
+            <tr><th>Description</th><th>Done</th></tr>
+          </thead>
+          <tbody>{ this.todoTableRows() }</tbody>
+        </table>
       </div>
     </div> 
 }
